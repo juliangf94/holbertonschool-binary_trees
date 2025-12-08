783985b7702208c43e62cebe95ef0695c13733db
cousinfabien <cousin.fabien3@gmail.com>
2025-12-08 13:03:51 +0000

    Fix Task 5

--- holbertonschool-binary_trees/5-binary_tree_is_root.c
+++ holbertonschool-binary_trees/5-binary_tree_is_root.c
@@ -8,13 +8,13 @@
 * Return: 1 if the node is a root, 0 if not
 */
 
-int binary_tree_is_root (const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
 
 	if (node->parent == NULL)
 		return (1);
-	
+
 	return (0);
 }
